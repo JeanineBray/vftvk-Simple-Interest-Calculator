37a75ecd792c52ef2ba5a7d4ea866af4a4b97fa7
JeanineBray <jeaninebray@outlook.com>
2021-09-12 14:02:40 -0400

    commit on master

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-function compute()
-{
+function compute(){
     var principal = document.getElementById("principal").value;
 
-    if(principal == "" || principal <= 0)
-    {
+    if(principal == "" || principal <= 0){
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return;
@@ -21,15 +19,12 @@ function compute()
     resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
 
-function updateRate() 
-{
+function updateRate() {
     var slider = document.getElementById("rate");
     var output = document.getElementById("rate_display");
-    output.innerHTML = slider.value; // Display the default slider value
+    output.innerHTML = slider.value;
 
-    // Update the current slider value (each time you drag the slider handle)
-    slider.oninput = function() 
-    {
+    slider.oninput = function() {
         output.innerHTML = this.value;
     }  
 }
